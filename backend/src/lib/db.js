5dd525787ec32242c83bdc4508f0f87c3eb71fac
NehaK-XP <nehakamath.241cs240@nitk.edu.in>
2026-06-09 01:09:10 +0530

    fix: validation added for database url

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,6 +3,9 @@ import { ENV } from "./env.js"
 
 export const  connectDB = async () => {
     try {
+        if(!ENV.DB_URL){
+            throw new Error("Database url not defined in environment variables!")
+        }
         const conn = await connect(ENV.DB_URL)
         console.log("Successfully connected to MongoDB", conn.connection.host)
     } catch (error) {
